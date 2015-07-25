1464c91fb72d4594501a3651bf386ca404f28815
Steve Domenico <stratcat944@gmail.com>
2015-07-25 10:09:15 -0600

    final

--- luminosity.js
+++ luminosity.js
@@ -1,15 +1,19 @@
-var r = process.argv[2]
-var g = process.argv[3]
-var b = process.argv[4]
 
-luminosity = 0.2126*r + 0.7152*g + 0.0722*b
+var red = process.argv[2];
+var green = process.argv[3];
+var blue = process.argv[4];
 
-// console.log(luminosity);
+var luminosity = function(){
+	return 0.2126*red + 0.7152*green + 0.0722*blue;
+	
+}
+console.log(luminosity());
 
-var isLight = luminosity > 155;
 
-if (isLight) {
-	console.log('light')	
-	} else {
-		console.log('dark')
-	}
+// var isLight = luminosity > 155;
+
+// if (isLight) {
+// 	console.log('light')	
+// 	} else {
+// 		console.log('dark')
+// 	}
